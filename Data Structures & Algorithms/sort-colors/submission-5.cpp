cfe2131a64548e599599c781ecd6f25af5de45c1
Sudarsan S <44109949+Burnfireblaze@users.noreply.github.com>
2026-06-12 00:40:56 -0700

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid =0;
+        while(mid <= right){
+            if (nums[mid] == 0){
+                swap(nums[left], nums[mid]);
+                mid +=1;
+                left += 1;
+            }
+            else if (nums[mid] == 1){
+                mid += 1;
+            }
+            else{
+                swap(nums[right], nums[mid]);
+                right -= 1;
+            }
+        }
+    }
+};
